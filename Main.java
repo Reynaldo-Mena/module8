f8926503a8da585ab1220cd185607343879d80f2
Reynaldo-Mena <134249465+Reynaldo-Mena@users.noreply.github.com>
2023-07-09 23:21:22 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package hw;
+
+import java.util.Random;
+
+public class Main {
+
+public static void main(String[] args){
+
+Random rand = new Random();
+
+int[] arr = new int[200000000];
+for (int i = 0; i < arr.length; i++) {
+arr[i] = rand.nextInt(10) + 1;
+}
+
+long start = System.currentTimeMillis();
+System.out.println(Concurrency.sum(arr));
+System.out.println("Single: " + (System.currentTimeMillis() - start));
+start = System.currentTimeMillis();
+System.out.println(Concurrency.parallelSum(arr));
+System.out.println("Parallel: " + (System.currentTimeMillis() - start));
+}
+}
